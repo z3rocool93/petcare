a0d1d7a9e723049962bd474fd57232c0f687ec79
z3rocool93 <pamatus93@gmail.com>
2026-01-13 14:20:57 -0300

    Fix: Eliminando instancia duplicada de Alpine

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
+//import Alpine from 'alpinejs';
 
-window.Alpine = Alpine;
+//window.Alpine = Alpine;
 
 // Iniciamos Alpine.js
-Alpine.start();
+//Alpine.start();
