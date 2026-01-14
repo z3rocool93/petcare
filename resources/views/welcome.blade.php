230d36063c573ba70d32a89c1d344ce27f7b050f
z3rocool93 <pamatus93@gmail.com>
2026-01-14 09:59:54 -0300

    fix: diseño en landing page/welcome.blade footer y menú en mobile v4.2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,6 @@
     <header>
         <nav class="bg-white border-gray-200 dark:bg-gray-900 border-b">
             <div class="max-w-screen-xl mx-auto p-4 flex items-center">
-
                 {{-- 1. LADO IZQUIERDO: Logo (Ocupa 1/4 del ancho) --}}
                 <div class="flex flex-1 justify-start">
                     <a href="/" class="flex items-center space-x-3">
